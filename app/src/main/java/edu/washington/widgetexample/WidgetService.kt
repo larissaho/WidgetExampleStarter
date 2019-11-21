ffd87a58f00d03f5c0619ffeffdf57e308508e1a
larissaho <lho12@uw.edu>
2019-11-21 12:18:08 -0800

    added expanded view

--- app/src/main/java/edu/washington/widgetexample/WidgetService.kt
+++ app/src/main/java/edu/washington/widgetexample/WidgetService.kt
@@ -5,11 +5,14 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
 import android.os.IBinder
+import android.provider.ContactsContract.Intents.Insert.ACTION
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
 import androidx.core.content.getSystemService
+import kotlinx.android.synthetic.main.layout_floating_widget.view.*
 
 class WidgetService : Service() {
 
@@ -40,6 +43,22 @@ class WidgetService : Service() {
         widgetView?.findViewById<ImageView>(R.id.iv_collapsed_close)?.setOnClickListener {
             stopSelf()
         }
+
+        widgetView?.findViewById<ImageView>(R.id.iv_expanded_close)?.setOnClickListener {
+            widgetView?.canvas_expanded?.visibility = View.GONE
+            widgetView?.canvas_collapsed?.visibility = View.VISIBLE
+
+        }
+
+        widgetView?.canvas_root?.setOnTouchListener(object: View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                if (event?.action == MotionEvent.ACTION_UP) {
+                    widgetView?.canvas_collapsed?.visibility = View.GONE
+                    widgetView?.canvas_expanded?.visibility = View.VISIBLE
+                }
+                return true
+            }
+        })
 
         return super.onStartCommand(intent, flags, startId)
     }
