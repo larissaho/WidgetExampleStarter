39517cf6c492783c1540393b05b2d6fabfe32e41
larissaho <lho12@uw.edu>
2019-11-21 12:03:22 -0800

    removes widget when pressed

--- app/src/main/java/edu/washington/widgetexample/WidgetService.kt
+++ app/src/main/java/edu/washington/widgetexample/WidgetService.kt
@@ -1,20 +1,51 @@
 package edu.washington.widgetexample
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.PixelFormat
 import android.os.IBinder
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
+import android.widget.ImageView
+import androidx.core.content.getSystemService
 
 class WidgetService : Service() {
+
+    var widgetView: View? = null
+    var windowManager: WindowManager? = null
+
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        widgetView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null)
+
+        val params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            0,
+            0,
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT
+        )
+
+        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowManager?.addView(widgetView, params)
+
+        widgetView?.findViewById<ImageView>(R.id.iv_collapsed_close)?.setOnClickListener {
+            stopSelf()
+        }
+
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
+        windowManager?.removeView(widgetView)
         super.onDestroy()
     }
 }
